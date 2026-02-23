81bab843e6d04c725bdc1bff75db35a02790b201
Filip Lindespång <fillebilla1@hotmail.com>
2026-02-23 17:04:44 +0100

    Uncertain about the end result without the hardware, so had to make drastic changes, will wait a bit until the project is built physically before coding any more

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <RTClib.h>
 #include <Arduino.h>
 #include <Adafruit_PWMServoDriver.h>
 #include <SPI.h>
-#include "oledScreen.h"
 // Define section
 #define SCREEN_W 128
 #define SCREEN_H 64
@@ -16,7 +14,6 @@
 
 // OLED, PCA, RTC objects
 Adafruit_SSD1306 display(SCREEN_W, SCREEN_H, &Wire, OLED_RESET);
-oledScreen playAnimation(display);
 
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
@@ -41,7 +38,37 @@ const uint8_t servo2Pin = 1;
 const uint8_t rgbRedPin = 2;
 const uint8_t rgbGreenPin = 3;
 const uint8_t rgbBluePin = 4;
+//servo patterns
+struct IdleMove {
+    int servo1Pos;
+    int servo2Pos;
+    uint32_t durationMs;
+};
+IdleMove idle_move[] = {
+    {60, 10, 3000},
+    {40, 60, 2000},
+    {90, 10, 5000},
+    {20, 90, 2000},
+};
+const uint8_t idle_move_count = sizeof(idle_move) / sizeof(idle_move[0]);
+uint8_t currentIdleMove = 0;
+uint32_t idleMoveStart = 0;
+void pickRandomIdleMove() {
+    uint8_t next;
+    do {
+        next = random(idle_move_count);
 
+    }while (next == currentIdleMove && idle_move_count > 1);
+    currentIdleMove = next;
+    idleMoveStart = millis();
+    servo1Pin.write(idle_move[currentIdleMove].servo1Pos);
+    servo2Pin.write(idle_move[currentIdleMove].servo2Pos);
+}
+void updateIdle() {
+    if (millis() - idleMoveStart >= idle_move[currentIdleMove].durationMs) {
+        pickRandomIdleMove();
+    }
+}
 // Error codes
 const unsigned short int DISP_ERR = 2;
 // Timing
@@ -72,7 +99,6 @@ void setup() {
         Serial.println(DISP_ERR);
         while (1);
     }
-    playAnimation.begin();
 
 
 
@@ -82,20 +108,14 @@ bool lastTilt = false;
 void loop() {
     bool tilted = readTiltDebounced();
     if (tilted && !lastTiltState) {
-        playAnimation.begin();
     }
     if (tilted) {
-        playAnimation.update();
     }
     lastTilt = tilted;
-
-playAnimation.update();
-
-
-
     // Mood logic
     switch (current_mood) {
         case IDLE:
+            updateIdle();
             break;
         case HAPPY:
             break;
